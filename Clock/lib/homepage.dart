39203cb2027faf93e5288aea1d65e7215ec205aa
Chris <pixeldev02@gmail.com>
2020-08-13 19:44:44 +0200

    second

--- Clock/lib/homepage.dart
+++ Clock/lib/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:Clock/clock_view.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 
 class HomepagePage extends StatefulWidget {
 
@@ -11,11 +13,55 @@ class _HomepagePageState extends State<HomepagePage> {
 
   @override
   Widget build(BuildContext context) {
+    var now = DateTime.now();
+    var formattedTime = DateFormat('HH:mm').format(now);
+    var formattedDate = DateFormat('EEE, d MMM').format(now);
+    var timezoneString = now.timeZoneOffset.toString().split('.').first;
+    var offsetSign = '';
+
+    if (!timezoneString.startsWith('-')) offsetSign = '+';
+    print(timezoneString);
+
     return Scaffold(
+      backgroundColor: Color(0xFF2D2F41),
       body: Container(
-        alignment: Alignment.center,
-        color: Color(0xFF2D2F41),
-        child: ClockView(),
+        padding: EdgeInsets.all(32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Clock',
+             style: TextStyle(color: Colors.white, fontSize: 24),
+             ),
+             SizedBox(height: 32),
+             Text(
+               formattedTime,
+             style: TextStyle(color: Colors.white, fontSize: 64),
+             ),
+             Text(
+                formattedDate,
+             style: TextStyle(color: Colors.white, fontSize: 20),
+             ),
+            ClockView(),
+            Text(
+              'Timezone',
+             style: TextStyle(color: Colors.white, fontSize: 24),
+             ),
+            SizedBox(height: 16),
+            Row(
+               children: <Widget>[
+                 Icon(Icons.language,
+                  color: Colors.white
+                  ),
+                  SizedBox(width: 16),
+                  Text(
+                    'UTC' + offsetSign + timezoneString,
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+               ],
+             )
+          ],
+        )
       )
     );
   }
